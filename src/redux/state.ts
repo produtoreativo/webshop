5e3389bec7cb3d9db942f44d07da02b3bc7361ed
Christiano Milfont <cmilfont@gmail.com>
2023-10-18 11:19:34 -0300

    nao lembro o que era

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -15,7 +15,7 @@ export const initialGlobalState: GlobalState = {
       }
     },
     auth: {
-      "accessToken": "eyJraWQiOiIxIiwiYWxnIjoiSFMyNTYifQ.eyJ1aWQiOjMsInV0eXBpZCI6MywiaWF0IjoxNjkyNDU5MjQ3LCJleHAiOjE2OTI0NjI4NDd9.lAUKNwpVkyW0PKEMs1Ud_G6z2gZzZNHxtnJboXZA-GM",
+      "accessToken": "eyJraWQiOiIxIiwiYWxnIjoiSFMyNTYifQ.eyJ1aWQiOjMsInV0eXBpZCI6MywiaWF0IjoxNjkyNzE0ODI1LCJleHAiOjE2OTI3MTg0MjV9.YtRbvKrdMoqIiG3nTs7dJoK537526B60xdzAVu7Cmpo",
       "createdAt": 1692454268106,
       "expireIn": "1h"
     },
